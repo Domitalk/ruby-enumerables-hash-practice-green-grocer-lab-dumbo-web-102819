5409287cdce0616b2b57f0cd79f01ec284f86919
Dominic Chu <dominic.m.chu@gmail.com>
2019-09-25 05:57:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -35,12 +35,12 @@ end
 
 def apply_clearance(cart)
   cart.each do |item|
-    
+
       if cart[item][:clearance] == true
         cart[item][:price] = (cart[item][:price] * 0.80).round(2)
       end
   end
-  
+
   cart
 end
 
